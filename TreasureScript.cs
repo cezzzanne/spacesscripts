926636a8244cdc031e16f41af198bd4a512cd7e4
Pablo Arellano <pablo.arellanos97@gmail.com>
2021-04-11 21:03:09 -0700

    made treasure dissappear

--- TreasureScript.cs
+++ TreasureScript.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Database;
+
+
 
 namespace Spaces {
     public class TreasureScript : MonoBehaviour {
         
         TreasureHuntScript huntScript;
+
+        void Start() {
+            DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+            reference.Child("treasure").ValueChanged += HandleValueChanged;
+        }
+
+        void HandleValueChanged(object sender, ValueChangedEventArgs args) {
+            if (args.DatabaseError != null) {
+                Debug.LogError(args.DatabaseError.Message);
+                return;
+            }
+            DataSnapshot snapshot = args.Snapshot;
+            if (!snapshot.Exists) {
+                Destroy(gameObject);
+            }
+        }
 
         public void SetHuntManager(TreasureHuntScript ths) {
             huntScript = ths;
